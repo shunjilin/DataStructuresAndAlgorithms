cb8045b07b54a813f7b7cb6e0fec6d7fd13f36b2
Shunji Lin <lgxshunji@gmail.com>
2019-08-10 19:03:28 +0900

    Remove namespace, clang-format

--- C++/01.AlgorithmicToolbox/Week1ProgrammingChallenges/MaxPairwiseProduct.hpp
+++ C++/01.AlgorithmicToolbox/Week1ProgrammingChallenges/MaxPairwiseProduct.hpp
@@ -1,28 +1,26 @@
 #ifndef MAX_PAIRWISE_PRODUCT_HPP
 #define MAX_PAIRWISE_PRODUCT_HPP
-#include <vector>
 #include <utility>
+#include <vector>
 
-namespace MPP {
-    std::pair<unsigned long long, unsigned long long>
-    getMaxPair(const std::vector<unsigned long long>& numbers) {
-        unsigned long long max = 0, next_max = 0;
-        for (auto number : numbers) {
-            if (max < number) {
-                next_max = max;
-                max = number;
-            } else if (next_max < number) {
-                next_max = number;
-            }
-        }
-        return std::make_pair(max, next_max);
+std::pair<unsigned long long, unsigned long long>
+getMaxPair(const std::vector<unsigned long long> &numbers) {
+  unsigned long long max = 0, next_max = 0;
+  for (auto number : numbers) {
+    if (max < number) {
+      next_max = max;
+      max = number;
+    } else if (next_max < number) {
+      next_max = number;
     }
-    
-    unsigned long long
-    maxPairwiseProduct(const std::vector<unsigned long long>& numbers) {
-        auto max_pair = getMaxPair(numbers);
-        return max_pair.first * max_pair.second;
-    }
+  }
+  return std::make_pair(max, next_max);
+}
+
+unsigned long long
+maxPairwiseProduct(const std::vector<unsigned long long> &numbers) {
+  auto max_pair = getMaxPair(numbers);
+  return max_pair.first * max_pair.second;
 }
 
 #endif
--- C++/01.AlgorithmicToolbox/Week1ProgrammingChallenges/MaxPairwiseProductUnitTest.cpp
+++ C++/01.AlgorithmicToolbox/Week1ProgrammingChallenges/MaxPairwiseProductUnitTest.cpp
@@ -1,28 +1,25 @@
-#include <gmock/gmock.h>
 #include "MaxPairwiseProduct.hpp"
+#include <gmock/gmock.h>
 
 using namespace testing;
-using namespace MPP;
 
-class MaxPairwiseProductInitialize: public Test {
+class MaxPairwiseProductInitialize : public Test {
 public:
-    std::vector<unsigned long long>
-    numbers = {21468, 16859, 82178, 70496, 82939, 44491};
+  std::vector<unsigned long long> numbers = {21468, 16859, 82178,
+                                             70496, 82939, 44491};
 };
 
 TEST_F(MaxPairwiseProductInitialize, MaxPairReturnsCorrectly) {
-    ASSERT_THAT(getMaxPair(numbers),
-                (std::pair<unsigned long long, unsigned long long>
-                 (82939, 82178)));
-                
+  ASSERT_THAT(
+      getMaxPair(numbers),
+      (std::pair<unsigned long long, unsigned long long>(82939, 82178)));
 }
 
 TEST_F(MaxPairwiseProductInitialize, MaxPairwiseProductReturnsCorrectProduct) {
-    ASSERT_THAT(maxPairwiseProduct(numbers), 6815761142);   
+  ASSERT_THAT(maxPairwiseProduct(numbers), 6815761142);
 }
 
 int main(int argc, char *argv[]) {
-    testing::InitGoogleMock(&argc, argv);
-    return RUN_ALL_TESTS();
+  testing::InitGoogleMock(&argc, argv);
+  return RUN_ALL_TESTS();
 }
-
