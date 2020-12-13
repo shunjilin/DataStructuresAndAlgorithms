434bd232b554d8be73b8de786bd62149e97f025d
Shunji Lin <lgxshunji@gmail.com>
2020-12-13 15:10:26 +0900

    fix quick sort three way partition

--- Javascript/src/01.AlgorithmicToolbox/Week4DivideAndConquer/quickSort.js
+++ Javascript/src/01.AlgorithmicToolbox/Week4DivideAndConquer/quickSort.js
@@ -3,38 +3,56 @@
  * Sorts a sequence of elements
  * Implements randomized quick sort with three way partition: less than, equal and more than
  * @param {Array.<number>} elements sequence of elements
- * @returns {Array.<number>} elements in non descending order
+ * sorts elements in place
  */
 export function quickSort(elements) {
   quickSortHelper(elements, 0, elements.length);
-  return elements;
 }
 
 function quickSortHelper(elements, startIndex, endIndex) {
   if (endIndex - startIndex <= 1) {
     return;
   }
+  const [equalStartIndex, equalEndIndex] = threeWayPartition(
+    elements,
+    startIndex,
+    endIndex
+  );
+  quickSortHelper(elements, startIndex, equalStartIndex);
+  quickSortHelper(elements, equalEndIndex + 1, endIndex);
+}
+
+// partition three ways: less than, equal, greater than random pivot element
+// return the start and end index (inclusive) of the equal segment of the three way partition
+function threeWayPartition(elements, startIndex, endIndex) {
   const pivotIndex = getPivotIndex(startIndex, endIndex);
-  let lessThanIndex = startIndex;
-  let greaterThanIndex = endIndex - 1;
-  let index = startIndex;
-  while (index <= greaterThanIndex) {
-    if (elements[index] < elements[pivotIndex]) {
-      swap(elements, index, lessThanIndex);
+  const pivotElement = elements[pivotIndex];
+  // move pivot element to first element
+  swap(elements, startIndex, pivotIndex);
+  let equalStartIndex = startIndex + 1;
+  let equalEndIndex = endIndex - 1;
+  let index = startIndex + 1;
+  while (index <= equalEndIndex) {
+    if (elements[index] < pivotElement) {
+      swap(elements, index, equalStartIndex);
       index += 1;
-      lessThanIndex += 1;
-    } else if (elements[index] > elements[pivotIndex]) {
-      swap(elements, index, greaterThanIndex);
-      greaterThanIndex -= 1;
+      equalStartIndex += 1;
+    } else if (elements[index] > pivotElement) {
+      swap(elements, index, equalEndIndex);
+      equalEndIndex -= 1;
     } else {
       // equal
       index += 1;
     }
   }
-  quickSortHelper(elements, startIndex, pivotIndex);
-  quickSortHelper(elements, pivotIndex, endIndex);
+  // move pivot element back to middle
+  equalStartIndex -= 1;
+  swap(elements, startIndex, equalStartIndex);
+  // return middle (equal) range index
+  return [equalStartIndex, equalEndIndex];
 }
 
+// get random pivot index
 function getPivotIndex(startIndex, endIndex) {
   return startIndex + Math.floor(Math.random() * (endIndex - startIndex));
 }
--- Javascript/src/01.AlgorithmicToolbox/Week4DivideAndConquer/quickSort.test.js
+++ Javascript/src/01.AlgorithmicToolbox/Week4DivideAndConquer/quickSort.test.js
@@ -5,6 +5,12 @@ describe("quickSort", () => {
     const randomElements = Array.from({ length: 100 }, () =>
       Math.floor(Math.random() * 100)
     );
-    expect(quickSort(randomElements)).toEqual(randomElements.sort());
+    quickSort(randomElements);
+    expect(
+      // check if sorted
+      randomElements.every(
+        (value, index, array) => index === 0 || array[index - 1] <= value
+      )
+    ).toBeTruthy();
   });
 });
