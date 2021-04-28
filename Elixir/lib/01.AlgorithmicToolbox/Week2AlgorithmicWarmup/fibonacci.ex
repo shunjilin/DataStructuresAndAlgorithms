7d92c1c1892c7b2bc3ca269415fb170ec95b5bf6
Shunji Lin <lgxshunji@gmail.com>
2021-04-29 01:59:39 +0900

    last digit of fibonacci

--- Elixir/lib/01.AlgorithmicToolbox/Week2AlgorithmicWarmup/fibonacci.ex
+++ Elixir/lib/01.AlgorithmicToolbox/Week2AlgorithmicWarmup/fibonacci.ex
@@ -17,13 +17,30 @@ defmodule DataStructuresAndAlgorithms.AlgorithmicToolBox.Week2AlgorithmicWarmup.
   """
   @spec fibonacci(non_neg_integer()) :: non_neg_integer()
   def fibonacci(n) do
-    do_fibonacci(n, 0, 1)
+    do_fibonacci(n)
   end
 
-  defp do_fibonacci(n, a, b) do
+  defp do_fibonacci(n, a \\ 0, b \\ 1) do
     case n do
       0 -> a
       _ -> do_fibonacci(n - 1, b, a + b)
     end
+  end
+
+  @doc """
+  # Last Digit of a Fibonacci Number
+
+  ## Examples
+  iex> fibonacci_last_digit(3)
+  2
+
+  iex> fibonacci_last_digit(331)
+  9
+  """
+  @spec fibonacci_last_digit(non_neg_integer()) :: non_neg_integer()
+  def fibonacci_last_digit(n) do
+    n
+    |> do_fibonacci()
+    |> Integer.mod(10)
   end
 end
